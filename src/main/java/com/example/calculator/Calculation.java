59220778eadedb8d57def75c78a94719826a2a15
wangxing <wangxing@jiuqi.com.cn>
2022-01-18 11:07:39 +0800

    静态改造

--- src/main/java/com/example/calculator/Calculation.java
+++ src/main/java/com/example/calculator/Calculation.java
@@ -14,14 +14,14 @@ public class Calculation {
 
     private static final Map<String, IFunction> FUNCTION_MAP = new HashMap<>();
 
-    {
+    static {
         ServiceLoader<IFunction> load = ServiceLoader.load(IFunction.class);
         for (IFunction iFunction : load) {
             FUNCTION_MAP.put(iFunction.getName().toUpperCase(Locale.ROOT), iFunction);
         }
     }
 
-    public Object exec(String exp) {
+    public static Object exec(String exp) {
         Expression root = new Expression();
         root.setText(exp);
         root.setFunctionMap(FUNCTION_MAP);
--- src/main/java/com/example/calculator/CalculationMain.java
+++ src/main/java/com/example/calculator/CalculationMain.java
@@ -5,16 +5,16 @@ import org.slf4j.LoggerFactory;
 
 public class CalculationMain {
     private static final Logger logger = LoggerFactory.getLogger(CalculationMain.class);
+
     public static void main(String[] args) {
-        Calculation calculation = new Calculation();
         String exp = "(2-1)*3+2+(3*(9-(5+2)*1))";
-        logger.info("exp is {}",exp );
-        Object exec = calculation.exec(exp);
+        logger.info("exp is {}", exp);
+        Object exec = Calculation.exec(exp);
         logger.info("my answer is {}", exec);
 
         String exp2 = "Max(8,Max(5,4))+plus100(max(3,9))";
-        logger.info("exp is {}",exp2 );
-        Object exec2 = calculation.exec(exp2);
+        logger.info("exp is {}", exp2);
+        Object exec2 = Calculation.exec(exp2);
         logger.info("my answer is {}", exec2);
 
     }
